c83c53a04824c035edcfb09b1ebb1b026bf93e76
Janusz Grabis <janusz@frameworks.pl>
2025-01-10 21:48:34 +0100

    missing files added

---
+++ src/Controller/AppController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController {
+
+    #[Route('/', name: 'home')]
+    public function home(): Response {
+        return $this->render('home.html.twig');
+    }
+
+}
